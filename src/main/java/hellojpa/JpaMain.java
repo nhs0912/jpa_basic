0d5a97e917524eeae4a9832ad7a5f338cc17a0c9
HeeSeok Noh <nhs0912@nate.com>
2021-10-23 01:07:38 +0900

    feat : 다양한 연관관계 매핑 다대다 까지

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,43 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.concurrent.locks.Lock;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        //code
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        try {
+            Member member = saveMember(entityManager);
+
+            Team team = new Team();
+            team.setName("teamA");
+            team.getMembers().add(member);
+
+            entityManager.persist(team);
+
+            Locker locker = new Locker();
+            locker.setName("testLocker");
+            entityManager.persist(locker);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+
+    private static Member saveMember(EntityManager entityManager) {
+        Member member = new Member();
+        member.setUserName("member1");
+        entityManager.persist(member);
+        return member;
+    }
+}
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,48 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+//@Table(name = "USER")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private String id;
+
+    @Column(name = "USERNAME", nullable = false)
+    private String userName;
+
+    @ManyToOne
+    //@JoinColumn(name="TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+}
